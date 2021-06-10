adb6c61bf83c87b74cba0dcbdb7e882c1aaedb20
yashshivhare911119@gmail.com <yashshivhare911119@gmail.com>
2021-06-10 12:00:33 +0530

    Second Commit

---
+++ Data/EmpDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApp2.Data
+{
+    public class EmpDbContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public EmpDbContext() : base("name=EmpDbContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WebApp2.Models.Emp> Emps { get; set; }
+    }
+}
